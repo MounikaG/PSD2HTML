5684394d8afd42553b745ea0c14cbcd83237f405
MounikaG <mouni.gunturu@gmail.com>
2015-09-23 16:25:01 +0530

    final pssd site

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,15 @@
-module.exports = function(grunt) {
+  module.exports = function(grunt) {
   grunt.initConfig({
               sass: {                              // Task 
                  dist: {                            // Target 
                    files: {                         // Dictionary of files 
                     'assets/stylesheets/common.css': 'assets/stylesheets/src/common.scss',  
-                    'assets/stylesheets/land.css': 'assets/stylesheets/src/land.scss',     
-                    }
+                    'assets/stylesheets/land.css': 'assets/stylesheets/src/land.scss', 
+                    'assets/stylesheets/about.css': 'assets/stylesheets/src/about.scss',
+                    'assets/stylesheets/how_works.css': 'assets/stylesheets/src/how_works.scss',
+                    'assets/stylesheets/services.css': 'assets/stylesheets/src/services.scss',
+                    'assets/stylesheets/contact.css': 'assets/stylesheets/src/contact.scss'
+                  }
                 }
               },
               watch:   {
@@ -23,17 +27,19 @@ module.exports = function(grunt) {
             cssmin: {
               target: {
                 files: {
-                'assets/stylesheets/app.min.css':['assets/stylesheets/common.css', 'assets/stylesheets/land.css']
-                  
-                   
-                  
-                 
+                'assets/stylesheets/app.min.css':
+                [
+                'assets/stylesheets/common.css', 
+                'assets/stylesheets/land.css',
+                'assets/stylesheets/about.css',
+                'assets/stylesheets/how_works.css',
+                'assets/stylesheets/services.css',
+                'assets/stylesheets/contact.css'
+                ]
                 }
               }
             }
-          });
-
-
+  });
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   // grunt.loadNpmTasks('grunt-contrib-concat');
